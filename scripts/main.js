0566423dec24abf93caadbe112c2a7eae83d811f
ananyamadhur <ananyamadhur123@gmail.com>
2020-05-11 16:51:43 +0200

    Final commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,26 @@
-function converter(lightYears) {
-	var lightyears = 100;
-	var miles = 5;
-	var result = lightyears * miles;
-	return result;
+function convertDistance(e) {
+    console.log(e);
+	var lightyears = parseFloat(e.target.value);
+	var miles = 5879*1000000000*lightyears;
+    document.getElementById('mi').value=miles;
+}
+function convertWeight(e) {
+    console.log(e);
+	var earth = parseFloat(document.getElementById('wc').value);
+	var mars = .38*earth;
+    document.getElementById('mw').value=mars;
+    document.getElementById('wm').style.color='lightgreen';
 }
 
-converter();
+var dist = 0;
+function updateDist () {
+    dist = dist + 36000;
+    document.getElementById('dtv').innerHTML=dist+' miles';
+   if (dist<139808518.253) setTimeout(updateDist,1000);
+    else alert('You have reached your destination');  
+}
+
 
 alert('It takes 300 days to reach Mars');
+document.getElementById('ly').addEventListener('keyup',convertDistance);
+
