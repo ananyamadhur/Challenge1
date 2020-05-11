cdf8daf114666d8beeb5ca03067958c455167101
ananyamadhur <ananyamadhur123@gmail.com>
2020-05-11 09:07:53 +0200

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+function converter(lightYears) {
+	var lightyears = 100;
+	var miles = 5;
+	var result = lightyears * miles;
+	return result;
+}
+
+converter();
+
+alert('It takes 300 days to reach Mars');
